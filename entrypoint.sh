7a42550b32813efff4de2d75269fb59a8ec1a806
Magnus Hamrin <mhamrin@deloitte.no>
2020-01-24 12:39:43 +0100

    Add sfdx cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@
 # The execution of this script stops if a command or pipeline has an error.
 # For example, failure to install a dependent package will cause the script
 # to stop execution.
+
+
+wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz
+mkdir sfdx-cli
+tar xJf sfdx-linux-amd64.tar.xz -C sfdx-cli --strip-components 1
+./sfdx-cli/install
+
 set -e
 
 # Specify the user name of the subscriber org.
